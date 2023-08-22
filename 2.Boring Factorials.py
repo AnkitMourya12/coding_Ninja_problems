fccc85aa00274441d50a7441d10f9662b2f4111a
ANKIT MOURYA <121748364+AnkitMourya12@users.noreply.github.com>
2023-08-22 22:06:16 +0530

    Ankit Mourya

---
+++ 2.Boring Factorials.py
@@ -0,0 +1,10 @@
+from os import *
+from sys import *
+from collections import *
+from math import *
+
+def boringFactorials(n, p):
+    # Write your code here.
+
+    return factorial(n)%p
+    pass
